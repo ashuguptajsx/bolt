2b8d073337772baf5e4430179cb8f39657e9c2f5
ashu-1403 <129203568+ashu-1403@users.noreply.github.com>
2025-02-02 01:35:55 +0530

    commit

---
+++ app/Provider.jsx
@@ -0,0 +1,21 @@
+"use client"
+import React from 'react'
+
+import { ThemeProvider as NextThemesProvider } from 'next-themes'
+
+
+function Provider({children}) {
+  return (
+    <div>
+        <NextThemesProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChang>
+        {children}
+        </NextThemesProvider>
+    </div>
+  )
+}
+
+export default Provider

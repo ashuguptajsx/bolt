9f38060f3373e855c31b84bef6b1486ed424b7d1
ashu-1403 <129203568+ashu-1403@users.noreply.github.com>
2025-02-03 01:01:40 +0530

    commit

--- components/Rcomponents/Hero.jsx
+++ components/Rcomponents/Hero.jsx
@@ -1,7 +1,12 @@
-import { ArrowRight } from "lucide-react";
+"use client";
+
+import { ArrowRight, Link } from "lucide-react";
 import React from "react";
+import { useState } from "react";
 
 const Hero = () => {
+
+  const[userInput, setUserInput] = useState("")
   return (
     <div className="flex flex-col items-center mt-36 xl:mt-52 gap-2">
       <h2 className="text-4xl font-bold ">What do you want to build?</h2>
@@ -9,10 +14,13 @@ const Hero = () => {
         Prompt,run,edit and deploy full-stack web apps
       </p>
       <div className="p-5 border rounded-xl max-w-2xl w-full">
-        <div className="flex gap-3">
-          <textarea placeholder="What you want to build" className="outline-none bg-transparent  h-32 max-h-56"/>
-          <ArrowRight className="bg-blue-500 p-2 h-8 w-8 rounded-md cursor-pointer" />
+        <div className="flex gap-2">
+          <textarea placeholder="What you want to build"
+          onChange = {(event) => setUserInput(event.target.value)}
+           className="outline-none bg-transparent w-full h-32 max-h-56 "/>
+         {userInput && <ArrowRight className="bg-blue-500 p-2 h-8 w-8 rounded-md cursor-pointer" />}
         </div>
+        <Link className="h-5 w-5"/>
       </div>
     </div>
   );

276aa862a0843c2892cef2815bd16b18cb3f5214
ashu-1403 <129203568+ashu-1403@users.noreply.github.com>
2025-02-23 22:48:41 +0530

    commit

--- convex/Schema.js
+++ convex/Schema.js
@@ -8,6 +8,7 @@ export default defineSchema({
         email:v.string(),
         picture:v.string(),
         uid:v.string(),
+        token:v.optional(v.number()),
     }),
     workspace:defineTable({
         messages:v.any(),
